840147a4d4a0763c263d25dd32471564c4b4d9f6
Andres Vasquez <andres.vasquez.a@hotmail.com>
2016-10-18 00:33:01 -0400

    Instrucciones

--- app/src/main/java/net/jakare/devlivery/controller/server/FirebasePedidosController.java
+++ app/src/main/java/net/jakare/devlivery/controller/server/FirebasePedidosController.java
@@ -2,17 +2,8 @@ package net.jakare.devlivery.controller.server;
 
 import android.app.Activity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import net.jakare.devlivery.R;
 import net.jakare.devlivery.model.dbClasses.Pedido;
-import net.jakare.devlivery.utils.constants.AppConstants;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,40 +26,26 @@ public class FirebasePedidosController {
     }
 
     public void CrearPedido(Pedido pedido){
-        //Firebase upload object
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child(AppConstants.TAG_PEDIDOS).push().setValue(pedido, new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                if(databaseError!=null){
-                    callBackGestion.onResponse(AppConstants.RESULTADO_INCORRECTO,
-                            databaseError.getMessage());
-                } else {
-                    callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,
-                            activity.getResources().getString(R.string.registro_correcto));
-                }
-            }
-        });
+        //TODO Enviar pedido
+        /*if(databaseError!=null){
+            callBackGestion.onResponse(AppConstants.RESULTADO_INCORRECTO,
+                    databaseError.getMessage());
+        } else {
+            callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,
+                    activity.getResources().getString(R.string.registro_correcto));
+        }*/
     }
 
     public void ListasPedidos(int estado){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child(AppConstants.TAG_PEDIDOS).orderByChild(AppConstants.TAG_ESTADO)
-                .equalTo(estado).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                List<Pedido> lstPedidos=new ArrayList<Pedido>();
-                for(DataSnapshot dataPedido : dataSnapshot.getChildren()){
-                    Pedido pedido=dataPedido.getValue(Pedido.class);
-                    pedido.setKey(dataPedido.getKey());
-                    lstPedidos.add(pedido);
-                }
-                callBackLista.onResponse(lstPedidos);
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+        //TODO Listar pedidos
+
+        /*List<Pedido> lstPedidos=new ArrayList<Pedido>();
+        for(DataSnapshot dataPedido : dataSnapshot.getChildren()){
+            Pedido pedido=dataPedido.getValue(Pedido.class);
+            pedido.setKey(dataPedido.getKey());
+            lstPedidos.add(pedido);
+        }
+        callBackLista.onResponse(lstPedidos);*/
     }
 
     public interface ResultadoGestion{
--- app/src/main/java/net/jakare/devlivery/controller/server/FirebaseProductosController.java
+++ app/src/main/java/net/jakare/devlivery/controller/server/FirebaseProductosController.java
@@ -2,27 +2,14 @@ package net.jakare.devlivery.controller.server;
 
 import android.app.Activity;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.util.Log;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.google.gson.Gson;
 
-import net.jakare.devlivery.R;
 import net.jakare.devlivery.model.dbClasses.Producto;
 import net.jakare.devlivery.utils.constants.AppConstants;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,41 +33,24 @@ public class FirebaseProductosController {
     }
 
     public void AdjuntarFoto(Uri uriFoto){
+        //TODO adjuntar foto
 
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReferenceFromUrl(AppConstants.TAG_STORAGE);
-        final StorageReference photoReference = storageReference.child(AppConstants.TAG_IMAGENES_PRODUCTOS).child(uriFoto.getLastPathSegment());
-        photoReference.putFile(uriFoto).addOnFailureListener(activity, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBackGestion.onResponse(AppConstants.RESULTADO_INCORRECTO,activity.getResources().getString(R.string.error_adjuntar));
-            }
-        }).addOnSuccessListener(activity, new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,
-                        taskSnapshot.getMetadata().getDownloadUrl().toString());
-            }
-        });
+        /*callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,
+                taskSnapshot.getMetadata().getDownloadUrl().toString());*/
     }
 
     public void CrearProducto(Producto producto){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child(AppConstants.TAG_PRODUCTO).push().setValue(producto, new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                if(databaseError!=null){
-                    callBackGestion.onResponse(AppConstants.RESULTADO_INCORRECTO, databaseError.getMessage());
-                } else {
-                    callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,activity.getResources().getString(R.string.registro_correcto));
-                }
-            }
-        });
+        //TODO Agregar pedido
+
+        /*if(databaseError!=null){
+            callBackGestion.onResponse(AppConstants.RESULTADO_INCORRECTO, databaseError.getMessage());
+        } else {
+            callBackGestion.onResponse(AppConstants.RESULTADO_CORRECTO,activity.getResources().getString(R.string.registro_correcto));
+        }*/
     }
 
     public void ListasProductos(String categoria){
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-
         Query ref;
         if(!categoria.isEmpty()){
             ref=databaseReference.child(AppConstants.TAG_PRODUCTO)
@@ -89,26 +59,18 @@ public class FirebaseProductosController {
             ref=databaseReference.child(AppConstants.TAG_PRODUCTO);
         }
 
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        //TODO Listar categoria
 
-                List<Producto> lstProductos=new ArrayList<Producto>();
-                for(DataSnapshot dataProducto : dataSnapshot.getChildren()){
-                    Producto producto=dataProducto.getValue(Producto.class);
-                    producto.setKey(dataProducto.getKey());
-                    lstProductos.add(producto);
-                }
+        /*List<Producto> lstProductos=new ArrayList<Producto>();
+        for(DataSnapshot dataProducto : dataSnapshot.getChildren()){
+            Producto producto=dataProducto.getValue(Producto.class);
+            producto.setKey(dataProducto.getKey());
+            lstProductos.add(producto);
+        }
 
-                Log.e("Categoria",new Gson().toJson(lstProductos));
+        Log.e("Categoria",new Gson().toJson(lstProductos));
 
-                callBackLista.onResponse(lstProductos);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+        callBackLista.onResponse(lstProductos);*/
     }
 
 
